0780104978e3828b24eb43d8415cc2ca912f1a09
erasmusmoodle <erasmusmoodle@gmail.com>
2020-07-04 17:39:14 +0200

    sys import

--- examples/fastspeech2/train_fastspeech2.py
+++ examples/fastspeech2/train_fastspeech2.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Train FastSpeech2."""
+import sys
 
 sys.path.append(".")
 sys.path.append("../")
@@ -32,7 +33,7 @@ import numpy as np
 import argparse
 import logging
 import os
-import sys
+
 
 
 
